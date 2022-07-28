46d10f819ad8998122617355ed19f5ceac1fb288
Alejandra Rodriguez <jacfalcon.better.practices@gmail.com>
2022-07-28 06:21:19 -0600

    Primer programa en C

---
+++ MiPrograma/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MiPrograma
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Este es mi primer programa 
+
+            /* Este tambien es un comentario
+             * pero de varias lineas*/
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Hispano!");
+            Console.ReadLine();
+            Console.WriteLine(5+5);
+        }        
+        
+    }
+}
+
+
