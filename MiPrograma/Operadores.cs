aafb7cd0c6c5a489a8dd09e596316fde98e1539b
Alejandra Rodriguez <jacfalcon.better.practices@gmail.com>
2022-08-02 11:17:50 -0600

    Operadores Booleanos

--- MiPrograma/Operadores.cs
+++ MiPrograma/Operadores.cs
@@ -14,11 +14,22 @@ namespace MiPrograma
             int division = a / b;
             int modulo = a % b;
 
+            bool isMayor = 90 > 80;
+            bool isMenor = 80 < 90;
+            bool isIgual = 90 == 80;
+            bool isDiferente = 90 != 80;
+            bool esMayoroIgual = 90 >= 90;
+
             Console.WriteLine(suma);
             Console.WriteLine(resta);
             Console.WriteLine(multiplicacion);
             Console.WriteLine(division);
             Console.WriteLine(modulo);
+            Console.WriteLine(isMayor);
+            Console.WriteLine(isMenor);
+            Console.WriteLine(isIgual);
+            Console.WriteLine(isDiferente);
+            Console.WriteLine(esMayoroIgual);
 
 
         }
